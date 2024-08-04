b808e3678fd406784afa04c958597acdd0503d3f
jpablopachar <jpablopachar1993@gmail.com>
2024-08-04 05:54:30 -0500

    Método para la creación de datos Faker

--- src/Persistence/OnlineCoursesDbContext.cs
+++ src/Persistence/OnlineCoursesDbContext.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -6,6 +7,11 @@ namespace Persistence
 {
     public class OnlineCoursesDbContext : DbContext
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Price>? Prices { get; set; }
+        public DbSet<Qualification>? Qualifications { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=OnlineCourses.db").LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information).EnableSensitiveDataLogging();
@@ -40,6 +46,51 @@ namespace Persistence
 
             // Se configura una relación de muchos a muchos entre Course e Instructor utilizando la entidad CourseInstructor, definiendo las claves foráneas y la clave primaria compuesta.
             modelBuilder.Entity<Course>().HasMany(c => c.Instructors).WithMany(c => c.Courses).UsingEntity<CourseInstructor>(a => a.HasOne(p => p.Instructor).WithMany(p => p.CourseInstructors).HasForeignKey(p => p.InstructorId), b => b.HasOne(p => p.Course).WithMany(p => p.CourseInstructors).HasForeignKey(p => p.CourseId), c => { c.HasKey(t => new { t.InstructorId, t.CourseId }); });
+
+            modelBuilder.Entity<Course>().HasData(LoadMasterData().Item1);
+            modelBuilder.Entity<Price>().HasData(LoadMasterData().Item2);
+            modelBuilder.Entity<Instructor>().HasData(LoadMasterData().Item3);
+        }
+
+        private Tuple<Course[], Price[], Instructor[]> LoadMasterData()
+        {
+            var courses = new List<Course>();
+            var faker = new Faker();
+
+            for (var i = 1; i < 10; i++)
+            {
+                var courseId = Guid.NewGuid();
+
+                courses.Add(new Course
+                {
+                    Id = courseId,
+                    Description = faker.Commerce.ProductDescription(),
+                    Title = faker.Commerce.ProductName(),
+                    PublicationDate = DateTime.UtcNow
+                });
+            }
+
+            var priceId = Guid.NewGuid();
+
+            var price = new Price
+            {
+                Id = priceId,
+                ActualPrice = 10.0m,
+                PromotionalPrice = 8.0m,
+                Name = "Regular Price"
+            };
+
+            var prices = new List<Price> { price };
+
+            var fakerInstructor = new Faker<Instructor>()
+                .RuleFor(i => i.Id, _ => Guid.NewGuid())
+                .RuleFor(i => i.FirstName, faker => faker.Name.FirstName())
+                .RuleFor(i => i.LastName, faker => faker.Name.LastName())
+                .RuleFor(i => i.AcademicDegree, faker => faker.Name.JobTitle());
+
+            var instructors = fakerInstructor.Generate(10);
+
+            return Tuple.Create(courses.ToArray(), prices.ToArray(), instructors.ToArray());
         }
     }
 }
