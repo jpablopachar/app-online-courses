947448d6e380b432804db5474634285004019d59
jpablopachar <jpablopachar1993@gmail.com>
2024-08-07 21:01:01 -0500

    Creación de clase semilla para generar data y se realiza migración

--- src/WebApi/Extensions/DataSeed.cs
+++ src/WebApi/Extensions/DataSeed.cs
@@ -1,12 +1,91 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Bogus;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Models;
 
 namespace WebApi.Extensions
 {
-    public class DataSeed
+    public static class DataSeed
     {
-        
+        public static async Task SeedDataAuthentication(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+
+            var service = scope.ServiceProvider;
+            var loggerFactory = service.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var context = service.GetRequiredService<OnlineCoursesDbContext>();
+
+                await context.Database.MigrateAsync();
+
+                var userManager = service.GetRequiredService<UserManager<AppUser>>();
+
+                if (!userManager.Users.Any())
+                {
+                    var userAdmin = new AppUser
+                    {
+                        FullName = "Admin",
+                        UserName = "admin",
+                        Email = "admin@yopmail.com"
+                    };
+
+                    await userManager.CreateAsync(userAdmin, "Password1234$");
+
+                    await userManager.AddToRoleAsync(userAdmin, CustomRoles.ADMIN);
+
+                    var userClient = new AppUser
+                    {
+                        FullName = "Juan Pablo Pachar",
+                        UserName = "jppachar",
+                        Email = "jppachar@yopmail.com"
+                    };
+
+                    await userManager.CreateAsync(userClient, "Password1234$");
+
+                    await userManager.AddToRoleAsync(userClient, CustomRoles.CLIENT);
+                }
+
+                var courses = await context.Courses.Take(10).Skip(0).ToListAsync();
+
+                if (!context.Set<CourseInstructor>().Any())
+                {
+                    var instructors = await context.Instructors.Take(10).Skip(0).ToListAsync();
+
+                    foreach (var course in courses)
+                    {
+                        course.Instructors = instructors;
+                    }
+                }
+
+                if (!context.Set<Qualification>().Any())
+                {
+                    foreach (var course in courses)
+                    {
+                        var fakerQualification = new Faker<Qualification>()
+                            .RuleFor(q => q.Id, _ => Guid.NewGuid())
+                            .RuleFor(q => q.Student, f => f.Name.FullName())
+                            .RuleFor(q => q.Comment, f => f.Commerce.ProductDescription())
+                            .RuleFor(q => q.Score, 5)
+                            .RuleFor(q => q.CourseId, course.Id);
+
+                        var qualifications = fakerQualification.Generate(10);
+
+                        context.AddRange(qualifications);
+                    }
+                }
+
+                await context.SaveChangesAsync();
+            }
+            catch (Exception exception)
+            {
+                var logger = loggerFactory.CreateLogger<OnlineCoursesDbContext>();
+
+                logger.LogError(exception.Message);
+            }
+        }
     }
 }
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,5 +1,8 @@
 using Application;
+using Microsoft.AspNetCore.Identity;
 using Persistence;
+using Persistence.Models;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddIdentityCore<AppUser>(option =>
+{
+    option.Password.RequireNonAlphanumeric = false;
+    option.User.RequireUniqueEmail = true;
+}).AddRoles<IdentityRole>().AddEntityFrameworkStores<OnlineCoursesDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +32,9 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
+
+await app.SeedDataAuthentication();
+
+app.MapControllers();
 
 app.Run();
